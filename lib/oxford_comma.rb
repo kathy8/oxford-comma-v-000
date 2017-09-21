29327d7ed4fbb488469a5554551c9688e4fd684a
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-21 17:36:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length == 1
   array.join ()
 elsif array.length == 2
   array.join(" and ")
-elsif array.length > 2
-  array[0..-2].join(", ") + ", and + array [-1]"
+elsif array.length < 3
+  array[0..-3].join(", ") + ", and + array [-1]"
 end
 end
