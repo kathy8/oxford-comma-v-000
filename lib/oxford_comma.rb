c05b7bba8666d2ce82dd0016ff23ca86d2601fca
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-21 17:38:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
 if array.length == 1
-  array.join ()
+  array.join
 elsif array.length == 2
   array.join(" and ")
 elsif array.length > 2
-  array[0..-2].join(", ") + ", and + array [-1]"
+  array[0..-2].join(", ") + ", and " + array [-1]"
 end
 end
